10519af1bc044fabdea1cd9fc0858fa745d13264
andriellycris <andriele.gomes.ferreira@escola.pr.gov.br>
2026-06-15 12:21:48 +0000

    projeto final

--- main.js
+++ main.js
@@ -1,16 +1,9 @@
-for (let i = 0; i < botoes.length; i++) {
-  botoes[i].onclick = function () {
-    for (let j = 0; j < botoes.length; j++) {
-      botoes[j].classList.remove("ativo");
-      textos[j].classList.remove("ativo");
-    }
-
-    botoes[i].classList.add("ativo");
-    textos[i].classList.add("ativo");
-  };
-}
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-let tempoAtual = new Date();
+const tempoObjetivo1 = new Date("2026-11-01T00:00");
+const tempoObjetivo2 = new Date("2026-11-15T00:00");
+const tempoObjetivo3 = new Date("2026-12-04T00:00");
+const tempoObjetivo4 = new Date("2026-09-07T00:00");
+const tempoObjetivo3 = new Date("2026-11-22T00:00");
+const tempoObjetivo4 = new Date("2026-11-11T00:00");
 
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
